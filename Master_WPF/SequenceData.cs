5f4b9c1be0795140ace8ac6970a43f1eb719a196
StevenGLee <lijt_001@sina.com>
2020-07-12 19:10:53 +0800

    完成了上位机基本功能的实现

---
+++ Master_WPF/SequenceData.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Master_WPF
+{
+    [Serializable]
+    public class SequenceData: HotSpotDatas
+    {
+        int maxCount;
+        int seqCount;
+        List<int> buffer;
+        int maximum, minimum;
+
+        public SequenceData(int MaxCount, int Maximum, int Minimum)
+        {
+            Clear();
+            maxCount = MaxCount;
+            maximum = Maximum;
+            minimum = Minimum;
+            buffer = new List<int>();
+            AddSequenceData(minimum);
+        }
+        public void AddSequenceData(int data)
+        {
+            buffer.Add(data);
+            if (seqCount > maxCount)
+            {
+                while (buffer.Count > maxCount)
+                    buffer.RemoveAt(0);
+                seqCount = maxCount;
+            }
+            else if(seqCount == maxCount)
+                buffer.RemoveAt(0);
+            else
+                seqCount++;
+            Clear();
+            for(int i=0;i<seqCount;i++)
+                Add(new HotspotData(((double)i)/(seqCount-1), ((double)(buffer[i]-minimum))/(maximum-minimum)));
+        }
+        public new void SaveTo(Uri uri)
+        {
+            SaveTo(uri.ToString());
+        }
+        /// <summary>
+        /// 保存至uriString（即文件名）。
+        /// </summary>
+        /// <param name="uriString"></param>
+        public new void SaveTo(string uriString)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream fileStream = File.Create(uriString);
+            bf.Serialize(fileStream, this);
+            fileStream.Close();
+        }
+        public new void ReadFrom(Uri uri)
+        {
+            ReadFrom(uri.ToString());
+        }
+        public new void ReadFrom(string uriString)
+        {
+            if (File.Exists(uriString))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                FileStream filestream = File.Open(uriString, FileMode.Open);
+                SequenceData temp = (SequenceData)bf.Deserialize(filestream);
+                Clear();
+                buffer.Clear();
+                foreach (HotspotData hotspotData in temp)
+                    Add(hotspotData);
+                maxCount = temp.maxCount;
+                maximum = temp.maximum;
+                minimum = temp.minimum;
+                buffer = temp.buffer;
+                seqCount = temp.seqCount;
+                filestream.Close();
+            }
+            else
+            {
+                //Debug.Log("不存在存档");
+                MessageBox.Show("不存在存档");
+            }
+        }
+
+    }
+}
---
+++ Master_WPF/ShowAdcData.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Master_WPF
+{
+    /// <summary>
+    /// ShowAdcData.xaml 的交互逻辑
+    /// </summary>
+    public partial class ShowAdcData : Window
+    {
+        public SequenceData AdcData 
+        {
+            set 
+            {
+                AdcGraph.HotspotDatas = value;
+            }
+        }
+        public ShowAdcData()
+        {
+            InitializeComponent();
+        }
+    }
+}
